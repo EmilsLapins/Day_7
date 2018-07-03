57fed24a087470e731153d88865e7458334d8a58
Emils Lapins <emilslapins@gmail.com>
2018-07-03 19:24:48 +0300

    Task 1 and 2 done

---
+++ Day7/Day7/Praktiskie.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    class Praktiskie
+    {
+        public void Pirmais()
+        {
+            //Izvadit summu skaitlu reizinajumam * 2 (skaitli no 1-9)
+            //t.i. (1*2) + (2*2) + (3*2) +...
+            Console.WriteLine("Ludzu ievadiet skaitli no 1 lidz 9");
+            int sakums = Convert.ToInt16(Console.ReadLine());
+
+            int Rez = 0;
+            for (int i = 1; i < sakums +1; i++)
+            {
+
+                Rez =  Rez + (i * 2);
+
+                Console.WriteLine("Test   " + Rez);
+            }
+            Console.WriteLine("...Done   " + Rez);
+            
+        }
+
+        public void Otrais()
+        {
+            //Likt cilvekam meginat uzminet skaitli. Ja skaitlis sakrit ar 11, tad pareizi
+            //Max meginajumu skaits - 5.
+            //Ja 5 reizes nesanak, tad izvadit pazinojumu. 
+
+            
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Ludzu miniet skaitli!");
+                int sakums = Convert.ToInt16(Console.ReadLine());
+
+
+                if (sakums == 11)
+                {
+                    Console.WriteLine("Pareizi!");
+                    break;
+                }
+                else 
+                {
+                    if (i == 5)
+                    {
+                        Console.WriteLine("Game Over!");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("<< Insert coin here>>");
+
+                        break;
+                    }
+                    Console.WriteLine("Meiginiet velreiz!");
+                    
+                }
+                
+            }
+        }
+
+       
+
+    }
+}
+
